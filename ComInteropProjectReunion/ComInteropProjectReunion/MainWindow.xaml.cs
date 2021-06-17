c5f9d9512dde69083ca6630c2993a2ce7db11991
Angela Zhang <angzhang@microsoft.com>
2021-06-16 23:20:32 -0700

    Add project files.

---
+++ ComInteropProjectReunion/ComInteropProjectReunion/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using WinRT;
+using WinRT.Interop;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ComInteropProjectReunion
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void userConsentVerifierButton_Click(object sender, RoutedEventArgs e)
+        {
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            await Windows.Security.Credentials.UI.UserConsentVerifierInterop.RequestVerificationForWindowAsync(hwnd, "Test Message"); 
+        }
+
+        private async void filePickerButton_Click(object sender, RoutedEventArgs e)
+        {
+            // casting to (Object) is a workaround - cswinrt issue #485 with .As<> 
+            //var hwnd = ((object)this).As<WinRT.Interop.IWindowNative>().WindowHandle;
+
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            var folderPicker = new FolderPicker();
+            WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, hwnd);
+            folderPicker.FileTypeFilter.Add("*");
+
+            var folder = await folderPicker.PickSingleFolderAsync();
+            //filePickerButton.Content = folder != null ? folder.Path : string.Empty;
+        }
+    }
+}
